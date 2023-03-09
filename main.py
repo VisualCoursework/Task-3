906ad0b9a6764f421b6d18f9dd0d4c6e2d731cb5
Connor Keevill <connor.keevill.19@gmail.com>
2023-03-09 14:24:42 +0000

    Add a test script to main which will add all images to the feature database (and therefore display each of them)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import os
+from FeatureDatabase import FeatureDatabase
+
 if __name__ == "__main__":
     print("Hello, World!")
+
+    db = FeatureDatabase(1, 1)
+
+    images = ["Training/png/" + imagepath for imagepath in os.listdir("Training/png/")]
+    print(images)
+
+    db.add_training_images(images)
