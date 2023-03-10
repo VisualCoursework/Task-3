e02ef9dfc775438f417f2c731a3bfd4ee07d4330
Connor Keevill <connor.keevill.19@gmail.com>
2023-03-10 14:48:07 +0000

    Add some comments

--- FeatureDatabase.py
+++ FeatureDatabase.py
@@ -7,14 +7,24 @@ class FeatureDatabase:
     """
     Stores the features that are identified in training images and identifies corresponding features in test images.
     """
-
     def __init__(self, octave_count: int, scale_levels: int):
+        """
+        Construct the database
+        :param octave_count:
+        :param scale_levels:
+        """
         self.featureExtractor = cv.SIFT_create()
 
         self.octave_count = octave_count
         self.scale_levels = scale_levels
 
     def add_training_images(self, images: List[ndarray]) -> None:
+        """
+        Accepts a list of images or image filepaths and performs SIFT feature extraction to find points. Stores this
+        information in the database.
+
+        :param images: a list of images or image filepaths.
+        """
         for image in images:
             if type(image) == str:
                 image = cv.imread(image)
