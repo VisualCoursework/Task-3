15f81b8d53f05f68ce832eba8dccf8aa91625462
Connor Keevill <connor.keevill.19@gmail.com>
2023-04-04 14:13:23 +0100

    Create evaluation function for annotations

--- main.py
+++ main.py
@@ -3,19 +3,37 @@ import cv2 as cv
 import utils
 from FeatureDatabase import FeatureDatabase
 
+OCTAVE_LAYERS = 3
+MATCH_THRESHOLD = 250
+POINTS_TO_MATCH = 15
+INCLUDE_ROTATIONS = False
+
 if __name__ == "__main__":
-    print("Hello, World!")
-
-    db = FeatureDatabase(1, 1)
-
+    # First create the lists of directories for images.
     trainingImages = [(cv.imread("Training/png/" + imagePath), utils.get_image_name_from_file_name(imagePath))
                       for imagePath in os.listdir("Training/png/")]
+
+    testImagesNoRotation = [(cv.imread("TestWithoutRotations/images/" + imagePath), utils.get_image_name_from_file_name(imagePath))
+                    for imagePath in os.listdir("TestWithoutRotations/images/")]
+
+    testImagesWithRotations = [(cv.imread("Task3AdditionalTestDataset/images/" + imagePath), utils.get_image_name_from_file_name(imagePath)) for imagePath in
+                    os.listdir("Task3AdditionalTestDataset/images/")]
+
+    # Create the database and add all the training images to it.
+    db = FeatureDatabase(OCTAVE_LAYERS, MATCH_THRESHOLD, POINTS_TO_MATCH)
     db.add_training_images(trainingImages)
 
-    testImages = [(cv.imread("TestWithoutRotations/images/" + imagePath), utils.get_image_name_from_file_name(imagePath))
-                  for imagePath in os.listdir("TestWithoutRotations/images/")]
-    # testImages += [(cv.imread("Task3AdditionalTestDataset/images/" + imagePath), imagePath) for imagePath in os.listdir("Task3AdditionalTestDataset/images/")]
-    db.print_annotations(testImages)
-    # db.show_matches_for_images(testImages)
+    # Now perform analysis of the test images.
+    for image, name in testImagesNoRotation:
+        # Read in the annotation for this test image
+        with open(f"TestWithoutRotations/annotations/{name}.txt") as f:
+            actual_annotation = f.read()
 
+        # Calcualte the annotation for the image based on the training data
+        predicted_annotation = db.get_annotation_for_image(image, name)
 
+        # Output the evaluation for this annotation.
+        evaluation = utils.evaluate_annotation(predicted_annotation, actual_annotation)
+        print(evaluation)
+
+        # db.show_boxes_around_images([(image, name)])
--- utils.py
+++ utils.py
@@ -1,4 +1,60 @@
-import re, functools
+import functools
+import re
+
+
+def evaluate_annotation(predicted: str, actual: str) -> dict:
+    """
+    Creates an evaluation dictionary for the two given annotations.
+
+    :param predicted: the annotation produced by the model.
+    :param actual: the correct annotation.
+    :return: the evaluation.
+    """
+    def convert_annotation_to_dict(annotation: str) -> dict:
+        """
+        Converts the given annotation (in the format that is being used for this assignment) into a dictionary of
+        coordinate pairs.
+
+        :param annotation: the annotation to convert.
+        :return: the dictionary.
+        """
+        output = {}
+
+        # First remove the brackets from the coords, so we can treat this as a CSV
+        annotation = annotation.replace("(", "")
+        annotation = annotation.replace(")", "")
+
+        for line in annotation.split("\n"):
+            if len(line) == 0: continue
+
+            line = line.split(",")
+
+            # A messy unpacking, but put a record in the output of the name of the image (line[0]) and store two tuples
+            # of the top left and bottom right coords.
+            output[line[0]] = [(line[1], line[2]), (line[3], line[4])]
+
+        return output
+
+    evaluation = {
+        "FPR": 0,
+        "TPR": 0,
+    }
+
+    predicted = convert_annotation_to_dict(predicted)
+    actual = convert_annotation_to_dict(actual)
+
+    for name, points in predicted.items():
+        # First handle the false positives
+        if name not in actual:
+            evaluation["FPR"] += 1
+        else:
+            evaluation["TPR"] += 1
+
+    evaluation["FNR"] = len(actual) - evaluation["TPR"]  # False negatives are any which are missed.
+    evaluation["TNR"] = 50 - len(predicted)
+    evaluation["ACC"] = (evaluation["TPR"] + evaluation["TNR"]) / (evaluation["TPR"] + evaluation["TNR"] + evaluation["FPR"] + evaluation["FNR"])
+
+    return evaluation
 
 def get_image_name_from_file_name(filename: str) -> str:
     """
